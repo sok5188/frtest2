5905b452529190c9d2936bcd2dbe3a36dc69992c
WONGYUN SHIN <43509682+sok5188@users.noreply.github.com>
2023-05-01 18:43:23 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 REPOSITORY=/home/ubuntu/testfe
 
 cd $REPOSITORY
+sudo npx pm2 kill
 
 sudo rm -rf node_modules
 
 sudo yarn install
 
-sudo npx pm2 reload all
+sudo npx pm2 start yarn --interpreter bash -- start
